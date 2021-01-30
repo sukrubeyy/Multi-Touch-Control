824c39848c4c5b220f38add628d580db7f1d1dae
sukrubeyy <sukru.beyy@outlook.com>
2021-01-31 01:44:15 +0300

    i did it

---
+++ multi-touch-control/Assets/Scripts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    Touch touch;
+    Vector3 touchPosition;
+    private void Update()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+        }
+    }
+}
